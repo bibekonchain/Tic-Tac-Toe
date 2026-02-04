15b70caefedee7b487c768776134e45c38b307dc
bibekkunwar <96648243+bibekonchain@users.noreply.github.com>
2026-02-04 22:10:53 +0545

    Refactor game logic and improve button handling

--- script.js
+++ script.js
@@ -5,100 +5,86 @@ let restartBtn = document.querySelector("#btn-restart");
 let msgRef = document.querySelector("#message");
 let turnX = document.querySelector("#X-turn");
 let turnO = document.querySelector("#O-turn");
-turnX.classList.add("glow");
 
-let winningPattern = [[0, 1, 2],
-[0, 3, 6],
-[2, 5, 8],
-[6, 7, 8],
-[3, 4, 5],
-[1, 4, 7],
-[0, 4, 8],
-[2, 4, 6]];
+let winningPattern = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 let xTurn = true;
 let count = 0;
-const winFunction = (character) => {
-    setTimeout(() => {
-        disabledButtons();
-        if (character === "X") {
-            msgRef.innerHTML = "&#x1f3c6;<br> 'X' Wins";
-        }
-        else {
-            msgRef.innerHTML = "&#x1f3c6;<br> 'O' Wins";
-        }
 
-    }, 300)
-};
-const winChecker = () => {
-    for (let arr of winningPattern) {
-        let [ele1, ele2, ele3] = [
-            btnRef[arr[0]].innerText,
-            btnRef[arr[1]].innerText,
-            btnRef[arr[2]].innerText
-        ];
-        if (ele1 !== "" && ele2 !== "" && ele3 !== "") {
-            if (ele1 === ele2 && ele2 === ele3) {
-                winFunction(ele1);
-                break;
-            };
-        };
-    };
-};
+// Highlight current turn
+turnX.classList.add("glow");
 
-const disabledButtons =() => {
-    btnRef.forEach((btn) => {
-        btn.disabled = true;
-    });
+// Disable all buttons and show popup
+const disableButtons = () => {
+    btnRef.forEach(btn => btn.disabled = true);
     popupRef.classList.remove("hide");
 };
-btnRef.forEach((element) => {
-    element.addEventListener("click", () => {
-        if (xTurn === true) {
-            element.innerText = "X";
-            xTurn = false;
-            turnO.classList.add("glow");
-            turnX.classList.remove("glow");
-        }
-        else {
-            element.innerText = "O";
-            xTurn = true;
-            turnO.classList.remove("glow");
-            turnX.classList.add("glow");
-        }
-        element.disabled = true;
-        count++;
-        if (count === 9) {
-           drawfunction()
-            return;
-        }
-        winChecker();
-    });
-});
 
-const drawfunction = () => {
-    disabledButtons();
-    msgRef.innerHTML="&#x1F38A; <br> It's a draw";
-}
-
-newGameBtn.addEventListener("click", ()=>{
-    count = 0;
-    xTurn = true;
-    turnX.classList.add("glow");
-    turnO.classList.remove("glow");
-    enableButtons();
-});
-const enableButtons =() => {
-    btnRef.forEach((btn) => {
+// Enable buttons and reset
+const enableButtons = () => {
+    btnRef.forEach(btn => {
         btn.disabled = false;
         btn.innerHTML = "";
+        btn.classList.remove("winner");
     });
-    popupRef.classList.add("hide");
-};
-restartBtn.addEventListener("click",()=>{
-    count = 0;
     xTurn = true;
+    count = 0;
     turnX.classList.add("glow");
     turnO.classList.remove("glow");
-    enableButtons();
+    popupRef.classList.add("hide");
+};
+
+// Check for win
+const checkWin = () => {
+    for (let arr of winningPattern) {
+        const [a, b, c] = arr.map(i => btnRef[i].innerText);
+        if (a && a === b && b === c) {
+            arr.forEach(i => btnRef[i].classList.add("winner"));
+            msgRef.innerHTML = `&#x1F3C6; <br> '${a}' Wins`;
+            disableButtons();
+            return true;
+        }
+    }
+    return false;
+};
+
+// Check for draw
+const checkDraw = () => {
+    if (count === 9 && !checkWin()) {
+        msgRef.innerHTML = `&#x1F38A; <br> It's a Draw`;
+        disableButtons();
+    }
+};
+
+// Handle button click
+btnRef.forEach(btn => {
+    btn.addEventListener("click", () => {
+        btn.innerText = xTurn ? "X" : "O";
+        btn.disabled = true;
+        count++;
+
+        if (!checkWin()) {
+            checkDraw();
+        }
+
+        // Toggle turn
+        xTurn = !xTurn;
+        turnX.classList.toggle("glow");
+        turnO.classList.toggle("glow");
+    });
 });
+
+// Event listeners
+newGameBtn.addEventListener("click", enableButtons);
+restartBtn.addEventListener("click", enableButtons);
+
 window.onload = enableButtons;
